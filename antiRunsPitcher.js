819a3c7b6a8b17d63e00c30415b013065e1a51e7
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-05-14 10:34:46 -0400

    runs

---
+++ antiRunsPitcher.js
@@ -0,0 +1,14 @@
+function calculateAntiRunsPitcher(stats) {
+    const IP = stats.inningsPitched || 0;
+    const ERA = stats.era || 5.00;  // Default high ERA
+    const WHIP = stats.whip || 1.5;
+    const K9 = stats.strikeoutsPer9Inn || 6;
+
+    const ERA_WEIGHT = 1;
+    const WHIP_WEIGHT = 10;
+    const K_WEIGHT = 0.5;
+
+    const leagueERA = 4.00;
+
+    return ((leagueERA - ERA) * IP * ERA_WEIGHT) + ((1 - WHIP) * WHIP_WEIGHT) + (K9 * K_WEIGHT);
+}
---
+++ runsValue.js
@@ -0,0 +1,12 @@
+function calculateRunsValue(stats) {
+    const RBI = stats.rbi || 0;
+    const OBP = stats.obp || 0;
+    const SB = stats.stolenBases || 0;
+    const XBH = (stats.doubles || 0) + (stats.triples || 0) + (stats.homeRuns || 0);
+
+    const OBP_WEIGHT = 10;
+    const SB_WEIGHT = 2;
+    const XBH_WEIGHT = 3;
+
+    return RBI + (OBP * OBP_WEIGHT) + (SB * SB_WEIGHT) + (XBH * XBH_WEIGHT);
+}
